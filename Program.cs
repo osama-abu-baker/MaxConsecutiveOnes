a86f1a63edd956452af1f8553590206c5dcc1e1b
OS-TECHNOLOGIES <os-technologies@localhost.localdomain>
2022-06-16 14:37:29 +0200

    Implment LeetCode example of MaxConsecutiveOnes

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Solution solution = new Solution();
+
+int[] numbers = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1};
+
+var result = solution.FindMaxConsecutiveOnes(numbers);
+
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int count = 0;
+        int[] counts = new int[nums.Length + 1];
+        int countsIndex = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                count += 1;
+            }
+            else
+            {
+                counts[countsIndex] = count;
+                countsIndex += 1;
+                count = 0;
+            }
+        }
+
+        counts[countsIndex] = count;
+
+        return counts.Max();
+    }
+}
